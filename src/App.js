49fca80e51ba29d7feea2d63f11fc00c00f37ebd
claudiocassimiro <days_surfing@live.com>
2021-12-08 19:59:39 -0300

    Adiciona rotas para as novas pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
-import Foods from './Pages/Foods';
+import FindFoods from './Pages/FindFoods';
+import FindDrinks from './Pages/FindDrinks';
+import Explorer from './Pages/Explorer';
+import ExploreDrink from './Pages/ExploreDrinks';
+import ExploreFoods from './Pages/ExploreFoods';
+import ExploreDrinksByIngredients from './Pages/ExploreDrinksByIngredients';
+import ExploreFoodsByIngredients from './Pages/ExploreFoodsByIngredients';
+import Profile from './Pages/Profile';
+import ExploreFoodsArea from './Pages/ExploreFoodsArea';
+import RecipesDone from './Pages/RecipesDone';
+import FavoriteRecipes from './Pages/FavoriteRecipes';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,7 +20,23 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/comidas" component={ Foods } />
+        <Route path="/comidas" component={ FindFoods } />
+        <Route path="/bebidas" component={ FindDrinks } />
+        <Route path="/explorar" component={ Explorer } />
+        <Route path="/explorar/comidas" component={ ExploreFoods } />
+        <Route path="/explorar/bebidas" component={ ExploreDrink } />
+        <Route
+          path="/explorar/comidas/ingredientes"
+          component={ ExploreFoodsByIngredients }
+        />
+        <Route
+          path="/explorar/bebidas/ingredientes"
+          component={ ExploreDrinksByIngredients }
+        />
+        <Route path="/explorar/comidas/area" component={ ExploreFoodsArea } />
+        <Route path="/perfil" component={ Profile } />
+        <Route path="/receitas-feitas" component={ RecipesDone } />
+        <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
       </Switch>
     </div>
   );
