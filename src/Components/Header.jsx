579330234caaa4f62608861d9df4b6f07243097d
claudiocassimiro <days_surfing@live.com>
2021-12-09 15:18:59 -0300

    Implementa logica condicional para renderizar o button search

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function Header() {
+function Header({ title, hasSearchButton = true }) {
   return (
     <header className="header">
-      <h1 data-testid="page-title">title</h1>
+      <h1 data-testid="page-title">{title}</h1>
       <button
         type="button"
-        data-testid="profile-top-btn"
       >
-        <img src={ profileIcon } alt="icone de perfil" />
+        <img data-testid="profile-top-btn" src={ profileIcon } alt="icone de perfil" />
       </button>
-      <button
-        type="button"
-        data-testid="search-top-btn"
-      >
-        <img src={ searchIcon } alt="icone de pesquisa" />
-      </button>
+      {hasSearchButton && (
+        <button
+          type="button"
+        >
+          <img data-testid="search-top-btn" src={ searchIcon } alt="icone de pesquisa" />
+        </button>
+      )}
     </header>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  hasSearchButton: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  hasSearchButton: true,
+};
 
 export default Header;
