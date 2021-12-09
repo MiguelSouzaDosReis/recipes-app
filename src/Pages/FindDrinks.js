2874c35c6be1893ee3b7862d79198d7b9d4b4a46
claudiocassimiro <days_surfing@live.com>
2021-12-09 19:14:40 -0300

    Adiciona logica para renderizar apenas 12 cards por vez

--- src/Pages/FindDrinks.js
+++ src/Pages/FindDrinks.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import Header from '../Components/Header';
+import AppContext from '../context/AppContext';
 
+const inicial = 0;
+const final = 11;
 function FindDrinks() {
+  const { drinks } = useContext(AppContext);
+  const [inicialIndex, setInicialIndex] = useState(inicial);
+  const [finalIndex, setFinalIndex] = useState(final);
+
+  const filterDrink = [];
+
+  drinks.forEach((drink, index) => {
+    if (index >= inicialIndex && index <= finalIndex) {
+      filterDrink.push(drink);
+    }
+  });
+
+  const onNextButtonClick = () => {
+    setInicialIndex(finalIndex + 1);
+    setFinalIndex(finalIndex + final + 1);
+  };
+
   return (
     <main>
       <Header title="Bebidas" />
-      <h1>Drinks</h1>
+      {(drinks.length > 0 || drinks !== null) && (
+        filterDrink.map((drink, index) => (
+          <article
+            data-testid={ `${index}-recipe-card` }
+            key={ drink.idDrink }
+          >
+            <h1
+              data-testid={ `${index}-card-name` }
+            >
+              { drink.strDrink }
+            </h1>
+            <img
+              src={ drink.strDrinkThumb }
+              alt={ drink.strDrink }
+              data-testid={ `${index}-card-img` }
+            />
+          </article>
+        ))
+      )}
+      <button
+        type="button"
+        onClick={ onNextButtonClick }
+      >
+        Próximo
+      </button>
     </main>
   );
 }
