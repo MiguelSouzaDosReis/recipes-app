723b9712ce622a0ad45e53f3ecc843666b7968bf
claudiocassimiro <days_surfing@live.com>
2021-12-09 17:01:52 -0300

    Implementa Api para requisição de drinks

---
+++ src/services/fetchTheCocktailAPI.js
@@ -0,0 +1,37 @@
+const searchByIngredient = async (ingredient) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+  const data = await response.json();
+  return data;
+};
+
+const searchByName = async (name) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+  const data = await response.json();
+  return data;
+};
+
+const searchByFirstLetter = async (firstLetter) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}`);
+  const data = await response.json();
+  return data;
+};
+
+const fetchTheCocktailAPI = async ({ ingredient, name, firstLetter }) => {
+  console.log(ingredient, name, firstLetter);
+  if (ingredient) {
+    const data = await searchByIngredient(ingredient);
+    return data;
+  }
+
+  if (name) {
+    const data = await searchByName(name);
+    return data;
+  }
+
+  if (firstLetter) {
+    const data = await searchByFirstLetter(firstLetter);
+    return data;
+  }
+};
+
+export default fetchTheCocktailAPI;
