db5524b8eae695a3f57ef7901784cec7ef4016a7
claudiocassimiro <days_surfing@live.com>
2021-12-10 20:01:18 -0300

    Implementa logicas para no useEffect

--- src/context/ProviderContext.js
+++ src/context/ProviderContext.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
+import fetchMealsDefault from '../services/fetchMealsDefault';
+import fetchDrinksDefault from '../services/fetchDrinksDefault';
+import fetchFoodCategories from '../services/fetchFoodCategories';
+import fetchDrinkCategories from '../services/fetchDrinksCategories';
+import fetchDrinksByCategory from '../services/fetchDrinksByCategory';
+import fetchFoodsByCategory from '../services/fetchFoodsByCategory';
 
 const ProviderContext = ({ children }) => {
   const [mealsToken, setMealsToken] = useState('');
@@ -8,6 +14,11 @@ const ProviderContext = ({ children }) => {
   const [user, setUser] = useState({ email: '' });
   const [drinks, setDrinks] = useState([]);
   const [meals, setMeals] = useState([]);
+  const [isFetchLoaded, setFetchLoaded] = useState(false);
+  const [foodCategories, setFoodCategories] = useState([]);
+  const [drinkCategories, setDrinkCategories] = useState([]);
+  const [drinkNameCategory, setDrinkNameCategory] = useState('');
+  const [mealNameCategory, setMealNameCategory] = useState('');
   /* const [doneRecipes, setDoneRecipes] = useState([[{
     id: '',
     type: '',
@@ -20,12 +31,61 @@ const ProviderContext = ({ children }) => {
     tags: '',
 }]]);
  */
+  useEffect(() => {
+    const fetchDefault = async () => {
+      setFetchLoaded(false);
+      const dataMeals = await fetchMealsDefault();
+      const dataDrinks = await fetchDrinksDefault();
+      setMeals(dataMeals);
+      setDrinks(dataDrinks);
+    };
+    fetchDefault().then(() => setFetchLoaded(true));
+  }, []);
 
   useEffect(() => {
     setMealsToken(localStorage.getItem('mealsToken'));
     setcocktailsToken(localStorage.getItem('cocktailsToken'));
     setUser(JSON.parse(localStorage.getItem('user')));
   }, []);
+
+  useEffect(() => {
+    const getFoodCategories = async () => {
+      const foodCategoriesList = await fetchFoodCategories();
+      setFoodCategories(foodCategoriesList);
+    };
+    getFoodCategories();
+  }, []);
+
+  useEffect(() => {
+    const getDrinksCategories = async () => {
+      const drinkCategoriesList = await fetchDrinkCategories();
+      setDrinkCategories(drinkCategoriesList);
+    };
+    getDrinksCategories();
+  }, []);
+
+  useEffect(() => {
+    const getDrinkCategory = async () => {
+      setFetchLoaded(false);
+      if (drinkNameCategory !== '') {
+        const drinkCategory = await fetchDrinksByCategory(drinkNameCategory);
+        setDrinks(drinkCategory);
+      }
+    };
+    getDrinkCategory().then(() => setFetchLoaded(true));
+  }, [drinkNameCategory]);
+
+  useEffect(() => {
+    const getMealCategory = async () => {
+      setFetchLoaded(false);
+      if (mealNameCategory !== '') {
+        const mealCategory = await fetchFoodsByCategory(mealNameCategory);
+        console.log(mealCategory);
+        setMeals(mealCategory);
+      }
+    };
+    getMealCategory().then(() => setFetchLoaded(true));
+  }, [mealNameCategory]);
 
   const setArrayMeals = (arrayMeals) => {
     setMeals(arrayMeals);
@@ -41,10 +101,15 @@ const ProviderContext = ({ children }) => {
     user,
     meals,
     drinks,
+    foodCategories,
+    drinkCategories,
+    setDrinkNameCategory,
+    setMealNameCategory,
     setMealsToken,
     setcocktailsToken,
     setArrayDrinks,
     setArrayMeals,
+    isFetchLoaded,
   };
 
   return (
