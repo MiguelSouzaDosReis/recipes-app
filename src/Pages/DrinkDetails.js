e4ff16c459fe7ff63e2c143fadcf344c51003abc
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-05 15:41:29 -0300

    Altera estado inicia
    Co-authored-by: Claudio Cassimiro <claudiocassimiro@users.noreply.github.com>
    Co-authored-by: Daniel Mendonça Acorroni <DanielAcorroni@users.noreply.github.com>
    Co-authored-by: Rafael Moura <rffmoura@users.noreply.github.com> de
    Co-authored-by: Miguel Reis <MiguelSouzaDosReis@users.noreply.github.com>
    
    Limita tamnaho recomendações

--- src/Pages/DrinkDetails.js
+++ src/Pages/DrinkDetails.js
@@ -9,7 +9,9 @@ function DrinkDetails() {
   const { currentDrinkRecipe, setCurrentDrinkRecipe } = useContext(AppContext);
   const [mealsRecomendation, setMealsRecomendation] = useState([]);
   const ingredientsArray = [];
-  const MAX_INGREDIENT_SIZE = 20;
+  const measureArray = [];
+  const MAX_INGREDIENT_SIZE = 15;
+  const RECOMENDATION_CARD_SIZE = 6;
   useEffect(() => {
     async function getRecipe() {
       const recipe = await fetchDrinkRecipe(slug);
@@ -29,9 +31,12 @@ function DrinkDetails() {
       if (currentDrinkRecipe[`strIngredient${i}`]) {
         ingredientsArray.push(currentDrinkRecipe[`strIngredient${i}`]);
       }
+      if (currentDrinkRecipe[`strIngredient${i}`]) {
+        measureArray.push(currentDrinkRecipe[`strMeasure${i}`]);
+      }
     }
   }
-
+  console.log(currentDrinkRecipe);
   return (
     <main>
       {currentDrinkRecipe && (
@@ -43,6 +48,7 @@ function DrinkDetails() {
             data-testid="recipe-photo"
           />
           <h2 data-testid="recipe-title">{currentDrinkRecipe.strDrink}</h2>
+          <h4 data-testid="recipe-category">{currentDrinkRecipe.strAlcoholic}</h4>
           <button
             data-testid="share-btn"
             type="button"
@@ -66,6 +72,8 @@ function DrinkDetails() {
                     data-testid={ `${index}-ingredient-name-and-measure` }
                   >
                     {ingredient}
+                    {' - '}
+                    {measureArray[index]}
                   </li>
                 ))
               }
@@ -73,26 +81,28 @@ function DrinkDetails() {
           </div>
           <p data-testid="instructions">{currentDrinkRecipe.strInstructions}</p>
           <div>
-            {mealsRecomendation && mealsRecomendation.map((drink, index) => (
-              <Link
-                key={ drink.idMeal }
-                to={ `/bebidas/${drink.idMeal}` }
-              >
-                <article
-                  data-testid={ `${index}-recomendation-card` }
+            {mealsRecomendation && mealsRecomendation.map((meal, index) => (
+              index < RECOMENDATION_CARD_SIZE && (
+                <Link
+                  key={ meal.idMeal }
+                  to={ `/comidas/${meal.idMeal}` }
                 >
-                  <h1
-                    data-testid={ `${index}-card-name` }
+                  <article
+                    data-testid={ `${index}-recomendation-card` }
                   >
-                    { drink.strMeal }
-                  </h1>
-                  <img
-                    src={ drink.strMealThumb }
-                    alt={ drink.strMeal }
-                    data-testid={ `${index}-card-img` }
-                  />
-                </article>
-              </Link>
+                    <h1
+                      data-testid={ `${index}-card-name` }
+                    >
+                      { meal.strMeal }
+                    </h1>
+                    <img
+                      src={ meal.strMealThumb }
+                      alt={ meal.strMeal }
+                      data-testid={ `${index}-card-img` }
+                    />
+                  </article>
+                </Link>
+              )
             ))}
           </div>
           <button
--- src/Pages/FoodDetails.js
+++ src/Pages/FoodDetails.js
@@ -7,27 +7,34 @@ import AppContext from '../context/AppContext';
 function FoodDetails() {
   const { slug } = useParams();
   const { currentMealRecipe, setCurrentMealRecipe } = useContext(AppContext);
-  const [drinksRecomendation, setDrinksRecomendation] = useState('');
+  const [drinksRecomendation, setDrinksRecomendation] = useState([]);
   const ingredientsArray = [];
+  const measureArray = [];
   const MAX_INGREDIENT_SIZE = 20;
+  const RECOMENDATION_CARD_SIZE = 6;
   useEffect(() => {
     async function getRecipe() {
       const recipe = await fetchFoodRecipe(slug);
+      const splitedLink = recipe.strYoutube.split('watch?v=');
+      const embedLink = `${splitedLink[0]}embed/${splitedLink[1]}`;
+      recipe.strYoutube = embedLink;
       setCurrentMealRecipe(recipe);
     }
     async function getDefaultRecomendations() {
       const defaultRecomendation = await fetchDrinksDefault();
       setDrinksRecomendation(defaultRecomendation);
-      console.log(defaultRecomendation);
     }
     getRecipe();
     getDefaultRecomendations();
   }, [setCurrentMealRecipe, slug]);
-
+  console.log(currentMealRecipe);
   if (currentMealRecipe) {
     for (let i = 1; i <= MAX_INGREDIENT_SIZE; i += 1) {
       if (currentMealRecipe[`strIngredient${i}`]) {
         ingredientsArray.push(currentMealRecipe[`strIngredient${i}`]);
+      }
+      if (currentMealRecipe[`strIngredient${i}`]) {
+        measureArray.push(currentMealRecipe[`strMeasure${i}`]);
       }
     }
   }
@@ -66,6 +73,8 @@ function FoodDetails() {
                     data-testid={ `${index}-ingredient-name-and-measure` }
                   >
                     {ingredient}
+                    {' - '}
+                    {measureArray[index]}
                   </li>
                 ))
               }
@@ -74,12 +83,11 @@ function FoodDetails() {
           <p data-testid="instructions">{currentMealRecipe.strInstructions}</p>
           <iframe
             data-testid="video"
-            id="player"
-            type="text/html"
-            width="640"
-            height="360"
+            width="1088"
+            height="612"
             src={ currentMealRecipe.strYoutube }
-            title={ currentMealRecipe.strMeal }
+            title="YouTube video player"
+            frameBorder="0"
             allow="accelerometer;
             autoplay;
             clipboard-write;
@@ -90,25 +98,27 @@ function FoodDetails() {
           />
           <div>
             {drinksRecomendation && drinksRecomendation.map((drink, index) => (
-              <Link
-                key={ drink.idDrink }
-                to={ `/bebidas/${drink.idDrink}` }
-              >
-                <article
-                  data-testid={ `${index}-recomendation-card` }
+              index < RECOMENDATION_CARD_SIZE && (
+                <Link
+                  key={ drink.idDrink }
+                  to={ `/bebidas/${drink.idDrink}` }
                 >
-                  <h1
-                    data-testid={ `${index}-card-name` }
+                  <article
+                    data-testid={ `${index}-recomendation-card` }
                   >
-                    { drink.strDrink }
-                  </h1>
-                  <img
-                    src={ drink.strDrinkThumb }
-                    alt={ drink.strDrink }
-                    data-testid={ `${index}-card-img` }
-                  />
-                </article>
-              </Link>
+                    <h1
+                      data-testid={ `${index}-card-name` }
+                    >
+                      { drink.strDrink }
+                    </h1>
+                    <img
+                      src={ drink.strDrinkThumb }
+                      alt={ drink.strDrink }
+                      data-testid={ `${index}-card-img` }
+                    />
+                  </article>
+                </Link>
+              )
             ))}
           </div>
           <button
