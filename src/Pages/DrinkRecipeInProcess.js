7d0b2a855185f723fc3fdcb1c3ec1b23aa34ecaf
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-01-07 20:11:16 -0300

    conserta req 10

--- src/Pages/DrinkRecipeInProcess.js
+++ src/Pages/DrinkRecipeInProcess.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import populateArrays from '../helpers/populateArrays';
 import fetchDrinkRecipes from '../services/fetchDrinkRecipes';
 import { setDrinkInProgress } from '../services/setRecipeInProgress';
 
@@ -49,20 +50,17 @@ function DrinkRecipeInProgress() {
   }, [setCurrentDrinkRecipe, drink]);
 
   if (currentDrinkRecipe) {
-    for (let i = 1; i <= MAX_INGREDIENT_SIZE; i += 1) {
-      if (currentDrinkRecipe[`strIngredient${i}`]) {
-        ingredientsArray.push(currentDrinkRecipe[`strIngredient${i}`]);
-      }
-      if (currentDrinkRecipe[`strIngredient${i}`]) {
-        measureArray.push(currentDrinkRecipe[`strMeasure${i}`]);
-      }
-    }
+    populateArrays(
+      currentDrinkRecipe, ingredientsArray, measureArray, MAX_INGREDIENT_SIZE,
+    );
   }
 
   useEffect(() => {
-    setDrinkInProgress({
-      id: currentDrinkRecipe.idDrink, ingredientsArray: ingredientStyle });
-  }, [ingredientStyle, currentDrinkRecipe.idDrink]);
+    if (currentDrinkRecipe) {
+      setDrinkInProgress({
+        id: currentDrinkRecipe.idDrink, ingredientsArray: ingredientStyle });
+    }
+  }, [ingredientStyle, currentDrinkRecipe]);
 
   return (
     <article>
