67d9ebbf1fe8cd8ad54d7d72ab2e61b216e5f19a
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-10 19:11:09 -0300

    Adiciona rotas no botão de Share

--- src/Pages/RecipesDone.js
+++ src/Pages/RecipesDone.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import ShareButton from '../Components/ShareButton';
 
 function RecipesDone() {
+  const [filter, setFilter] = useState('All');
   const doneRecipes = (!localStorage.getItem('doneRecipes')) ? []
     : JSON.parse(localStorage.getItem('doneRecipes'));
   return (
@@ -11,40 +13,53 @@ function RecipesDone() {
       <button
         type="button"
         data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('All') }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('comida') }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('bebida') }
       >
         Drink
       </button>
 
-      {doneRecipes.map((element, index) => (
+      {doneRecipes.filter((recipeFitered) => {
+        if (filter === 'All') {
+          return recipeFitered.type !== 'All';
+        }
+        return recipeFitered.type === filter;
+      }).map((element, index) => (
         <>
-          <img
-            key={ index }
-            alt={ element.id }
-            data-testid={ `${index}-horizontal-image` }
-            src={ element.image }
-          />
-          <h2 data-testid={ `${index}-horizontal-top-text` }>
-            {element.alcoholicOrNot ? element.alcoholicOrNot : (
-              `${element.area} - ${element.category}`
-            )}
-          </h2>
-          <h1
-            data-testid={ `${index}-horizontal-name` }
+          <Link
+            to={ `/${element.type}s/${element.id}` }
           >
-            { element.name }
-          </h1>
+            <img
+              width="300px"
+              key={ index }
+              alt={ element.id }
+              data-testid={ `${index}-horizontal-image` }
+              src={ element.image }
+            />
+            <h2 data-testid={ `${index}-horizontal-top-text` }>
+              {element.alcoholicOrNot ? element.alcoholicOrNot : (
+                `${element.area} - ${element.category}`
+              )}
+            </h2>
+            <h1
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { element.name }
+            </h1>
+          </Link>
           <h2
             data-testid={ `${index}-horizontal-done-date` }
           >
@@ -52,9 +67,9 @@ function RecipesDone() {
 
           </h2>
           <ShareButton
-            dataTestId={ `${index}-horizontal-share-btn` }
             type={ element.type }
             id={ element.id }
+            index={ index }
           />
           { element.tags.map((tag) => (
             <p
