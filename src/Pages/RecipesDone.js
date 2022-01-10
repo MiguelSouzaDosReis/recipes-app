000e5d104bb6c5bd6681d6c5dc00c20fabc50519
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-10 13:26:40 -0300

    Adiciona botões

--- src/Pages/RecipesDone.js
+++ src/Pages/RecipesDone.js
@@ -5,7 +5,24 @@ function RecipesDone() {
   return (
     <main>
       <Header title="Receitas Feitas" hasSearchButton={ false } />
-      <h1>Recipes Done</h1>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drink
+      </button>
     </main>
   );
 }
